525026c98d4466f418f97707f9dc3c15a7ab0e65
Liopyu <mistakensound@gmail.com>
2024-04-01 14:43:16 -0400

    initial commit, registered worldjs plugin

---
+++ src/main/java/net/liopyu/worldjs/WorldJS.java
@@ -0,0 +1,8 @@
+package net.liopyu.worldjs;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(WorldJS.MODID)
+public class WorldJS {
+    public static final String MODID = "worldjs";
+}
---
+++ src/main/java/net/liopyu/worldjs/WorldJSPlugin.java
@@ -0,0 +1,7 @@
+package net.liopyu.worldjs;
+
+import dev.latvian.mods.kubejs.KubeJSPlugin;
+
+public class WorldJSPlugin extends KubeJSPlugin {
+
+}
